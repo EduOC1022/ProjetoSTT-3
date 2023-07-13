e69ae395390e9802156324951f92728682f9e1af
EduOC1022 <112728180+EduOC1022@users.noreply.github.com>
2023-07-13 09:43:17 -0300

    bd

--- backend/index.js
+++ backend/index.js
@@ -1,9 +1,35 @@
-let http = require('http');
-var url = require('url');
-var dt = require('./data');
+const express = require('express');
+const {Pool} = require('pg');
+const app = express();
+const bd = new Pool({
+    user: 'stt',
+    password: 'stt123',
+    host: 'localhost',
+    port: 5432,
+    database: 'sttproject'
+});
 
-http.createServer(function (req, res) {
-  res.writeHead(200, {'Content-Type': 'text/html'});
-  res.write("The date and time are currently: " + dt.myDateTime());
-  res.end();
-}).listen(8080);
+bd.connect();
+console.log("Cliente foi conectado");
+
+bd.query('SELECT * FROM carros').then(results => {
+    const resultados = results.rows;
+    console.table(resultados);
+});
+
+app.get('/db', async (req, res) => {
+    try {
+      const pesquisa = 'SELECT * FROM carros';
+      const resultados = await bd.query(pesquisa);
+      const carros = resultados.rows;
+
+      res.json(carros);
+    } catch (ex) {
+      console.log('Erro ao recuperar dados do banco de dados:', ex);
+      res.status(500).send('Erro ao recuperar dados do banco de dados.');
+    }
+  });
+
+  app.listen(3000, () => {
+    console.log('Servidor iniciado na porta 3000.');
+});
